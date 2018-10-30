e33bbc95aa981dcbf0e1024626efbf2f056c6406
Andrey Miskov <amiskov@oggettoweb.com>
2018-10-30 20:27:42 +0300

    Add files for 1st lecture

---
+++ .vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    title: 'Frontend Lectures',
+    description: '',
+    base: '/frontend-lectures/',
+    themeConfig: {
+        sidebar: [{
+            title: 'В первую очередь',
+            collapsable: false,
+            children: [
+                '1-intro/'
+            ]
+        }]
+    }
+}
